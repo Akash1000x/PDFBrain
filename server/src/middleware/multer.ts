7c663628b251ee39b36fc6eb4136b45e7cf38488
akash <kumawatap63@gmail.com>
2025-11-18 23:55:26 +0530

    server: add multer middleware

---
+++ server/src/middleware/multer.ts
@@ -0,0 +1,15 @@
+import multer from "multer";
+import path from "path";
+import { getRootDirname } from "../utils/paths.js";
+
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, path.join(getRootDirname(), "uploads"));
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}-${file.originalname.replace(/[^a-zA-Z0-9.-]/g, '_')}`);
+  }
+})
+
+export const upload = multer({ storage });
