daebf5e9a5e9176d8a2162775f3edb5b4e8f68ea
akash <kumawatap63@gmail.com>
2025-11-19 21:53:02 +0530

    client: add chat component

---
+++ client/components/chat.tsx
@@ -0,0 +1,81 @@
+"use client";
+
+import { useChat } from "@ai-sdk/react";
+import { TextStreamChatTransport } from "ai";
+import PromptInput from "./prompt-input";
+import * as React from "react";
+import Markdown from "./markdown";
+import { Skeleton } from "./ui/skeleton";
+import { API_URL } from "@/lib/utils";
+import { getCurrentFileName } from "@/lib/storage";
+import { toast } from "sonner";
+import { File } from "lucide-react";
+
+export default function Page() {
+  const [message, setMessage] = React.useState<string>("");
+  const fileName = getCurrentFileName();
+  const { messages, sendMessage, status } = useChat({
+    transport: new TextStreamChatTransport({
+      api: `${API_URL}/chat`,
+      body: {
+        fileName,
+      },
+    }),
+  });
+  const handleSubmit = (data: { message: string }) => {
+    if (!fileName) {
+      toast.error("Please select a file or upload a new one");
+      return;
+    }
+    sendMessage({ text: data.message });
+  };
+
+  return (
+    <div className="h-screen relative">
+      {fileName && (
+        <div className="flex items-center gap-2 p-2 border w-fit rounded-md fixed top-2 left-2 bg-secondary z-20">
+          <File />
+          <p>{fileName?.split("_").slice(1).join(" ")}</p>
+        </div>
+      )}
+      <div className="space-y-10 lg:w-4xl w-xl mx-auto px-3 pt-8 pb-44">
+        {messages?.map((message, i) => (
+          <div key={`${message.id + i}`} className="">
+            {message.role === "user" ? (
+              <div className="group">
+                <div className="p-4 bg-muted rounded-md max-w-xl ml-auto text-justify">
+                  {message.parts[0].type === "text" ? message.parts[0].text : message.parts[0].type}
+                </div>
+              </div>
+            ) : (
+              message.parts.map((part, index) => (
+                <div key={`${message.id}-${index}`} className="">
+                  {part.type === "text" ? <MemoizedReactMarkdown>{part.text}</MemoizedReactMarkdown> : null}
+                </div>
+              ))
+            )}
+          </div>
+        ))}
+        {status === "submitted" && (
+          <div className="flex gap-2">
+            <Skeleton className="size-3" />
+            <Skeleton className="size-3" />
+            <Skeleton className="size-3" />
+          </div>
+        )}
+      </div>
+      <PromptInput
+        onSubmit={handleSubmit}
+        message={message}
+        setMessage={setMessage}
+        disabled={false}
+        selectedFileName={fileName ?? undefined}
+      />
+    </div>
+  );
+}
+
+const MemoizedReactMarkdown: React.FC<{ children: string }> = React.memo(
+  Markdown,
+  (prevProps, nextProps) => prevProps.children === nextProps.children
+);
