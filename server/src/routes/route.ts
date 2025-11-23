f4fd77c2794341cbd05f620464fc5dcc89f3b36c
akash <kumawatap63@gmail.com>
2025-11-23 16:59:13 +0530

    server: added multi query rag implementation

--- server/src/routes/route.ts
+++ server/src/routes/route.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express'
 import { uploadPdfController } from '../controllers/upload.controller.js';
 import { upload } from '../middleware/multer.js';
-import { chatController } from '../controllers/chat.controller.js';
+import { simpleRagChatController, multiQueryRagChatController } from '../controllers/chat.controller.js';
 
 const router: Router = Router()
 
 
 router.post("/upload", upload.single("pdf"), uploadPdfController)
-router.post("/chat", chatController)
+router.post("/simple-rag-chat", simpleRagChatController)
+router.post("/multi-query-rag-chat", multiQueryRagChatController)
 
 export default router;
